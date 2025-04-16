6a6b8ab2f4e968da526b1f5ce28ab48a93301ef4
Jennyfer Alves <jennyferpinheiro65@gmail.com>
2025-04-16 13:39:36 -0300

    Projeto CRUD

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+
+function App() {
+  const[btnCadastrar, setBtnCadastrar] = useState(false);
+
+  return (
+    <div>
+      <Formulario botao={btnCadastrar}/>
+      <Tabela/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/frontend/src/Formulario.js
@@ -0,0 +1,23 @@
+function Formulario({botao}){
+    return(
+        <form>
+            <h1>Formulário</h1>
+            <input className="form-control" type='text' placeholder='Nome'/>
+            <input className="form-control" type='text' placeholder='Marca'/>
+        {
+           botao
+            ?
+            <input className= "btn btn-success" type='button' value='Cadastrar'/>
+            :
+            <div>
+                <input className= "btn btn-primary" type='button' value='Alterar'/>
+                <input className= "btn btn-warning" type='button' value='Cancelar'/>
+                <input className= "btn btn-danger" type='button' value='Remover'/>
+            </div>
+        }
+            
+        </form>
+    )
+}
+
+export default Formulario;
