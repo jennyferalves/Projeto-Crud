869dd7bac4e28405e4abe45543970d97dabf8fba
Jennyfer Alves <jennyferpinheiro65@gmail.com>
2025-04-29 08:03:37 -0300

    Front conectado ao back

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -4,10 +4,18 @@ import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
-  const[btnCadastrar, setBtnCadastrar] = useState(false);
+  const[btnCadastrar, setBtnCadastrar] = useState(true);
+  const[produtos, setProdutos] =useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
 
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar}/>
       <Tabela/>
     </div>
